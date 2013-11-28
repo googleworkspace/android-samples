34cb151ce4819f1ef0c0860aee71dd7291d163aa
Burcu Dogan <burcujdogan@gmail.com>
2013-11-28 16:35:00 +0100

    Adding license headers and reorganizing imports.

--- src/com/google/android/gms/drive/sample/demo/CreateFileInFolderActivity.java
+++ src/com/google/android/gms/drive/sample/demo/CreateFileInFolderActivity.java
@@ -1,5 +1,19 @@
+/**
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.google.android.gms.drive.sample.demo;
+import android.os.Bundle;
 
 import com.google.android.gms.drive.Drive;
 import com.google.android.gms.drive.DriveApi.ContentsResult;
@@ -10,7 +24,6 @@ import com.google.android.gms.drive.DriveFolder.OnCreateFileCallback;
 import com.google.android.gms.drive.DriveId;
 import com.google.android.gms.drive.MetadataChangeSet;
 
-import android.os.Bundle;
 
 /**
  * An activity to create a file inside a folder.
@@ -18,7 +31,8 @@ import android.os.Bundle;
 public class CreateFileInFolderActivity extends BaseDemoActivity implements OnCreateFileCallback,
         OnNewContentsCallback {
 
-    private static DriveId sFolderId = DriveId.createFromResourceId("0B2EEtIjPUdX6MERsWlYxN3J6RU0");
+    private static final DriveId sFolderId =
+            DriveId.createFromResourceId("0B2EEtIjPUdX6MERsWlYxN3J6RU0");
 
     @Override
     public void onConnected(Bundle connectionHint) {
--- src/com/google/android/gms/drive/sample/demo/ListFilesInFolderActivity.java
+++ src/com/google/android/gms/drive/sample/demo/ListFilesInFolderActivity.java
@@ -1,14 +1,27 @@
+/**
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.google.android.gms.drive.sample.demo;
+
+import android.os.Bundle;
+import android.widget.ListView;
 
 import com.google.android.gms.drive.Drive;
 import com.google.android.gms.drive.DriveApi.MetadataBufferResult;
 import com.google.android.gms.drive.DriveFolder;
 import com.google.android.gms.drive.DriveFolder.OnChildrenRetrievedCallback;
 import com.google.android.gms.drive.DriveId;
-
-import android.os.Bundle;
-import android.widget.ListView;
 
 /**
  * An activity illustrates how to list files in a folder. For an example of
@@ -17,7 +30,8 @@ import android.widget.ListView;
 public class ListFilesInFolderActivity extends BaseDemoActivity implements
         OnChildrenRetrievedCallback {
 
-    private static DriveId sFolderId = DriveId.createFromResourceId("0B2EEtIjPUdX6MERsWlYxN3J6RU0");
+    private static final DriveId sFolderId =
+            DriveId.createFromResourceId("0B2EEtIjPUdX6MERsWlYxN3J6RU0");
 
     private ListView mResultsListView;
     private ResultsAdapter mResultsAdapter;
