38184fbaea62a7ccd1c510d0fbb78c652407920a
Ali <afshar@google.com>
2013-12-18 14:15:21 -0800

    Add some more actions to the sync request example.

--- src/com/google/android/gms/drive/sample/demo/SyncRequestsActivity.java
+++ src/com/google/android/gms/drive/sample/demo/SyncRequestsActivity.java
@@ -14,19 +14,24 @@
 
 package com.google.android.gms.drive.sample.demo;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
 
+import com.google.android.gms.drive.Contents;
 import com.google.android.gms.drive.Drive;
 import com.google.android.gms.drive.DriveApi.ContentsResult;
-import com.google.android.gms.drive.DriveFile;
 import com.google.android.gms.drive.DriveFolder;
 import com.google.android.gms.drive.DriveFolder.DriveFileResult;
+import com.google.android.gms.drive.DriveResource.MetadataResult;
+import com.google.android.gms.drive.Metadata;
 import com.google.android.gms.drive.MetadataChangeSet;
 
 /**
- * An activity to illustrate making synchronous requests to
- * the Drive service backend.
+ * An activity to illustrate making synchronous requests to the Drive service
+ * backend.
  */
 public class SyncRequestsActivity extends BaseDemoActivity {
 
@@ -39,38 +44,74 @@ public class SyncRequestsActivity extends BaseDemoActivity {
     }
 
     /**
-     * An async task that creates a new text file by
-     * creating new contents and metadata entities on user's root folder.
+     * An async task that creates a new text file by creating new contents and
+     * metadata entities on user's root folder. A number of blocking tasks are
+     * performed serially in a thread. Each time, await() is called on the
+     * result which blocks until the request has been completed.
      */
-    public class CreateFileAsyncTask extends AsyncTask<Void, Void, DriveFile> {
+    public class CreateFileAsyncTask extends AsyncTask<Void, Void, Metadata> {
 
         @Override
-        protected DriveFile doInBackground(Void... arg0) {
+        protected Metadata doInBackground(Void... arg0) {
+
+            // First we start by creating a new contents, and blocking on the
+            // result by calling await().
             ContentsResult contentsResult =
                     Drive.DriveApi.newContents(getGoogleApiClient()).await();
             if (!contentsResult.getStatus().isSuccess()) {
+                // We failed, stop the task and return.
                 return null;
             }
-            // create a new text file with empty contents
-            MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
-                    .setTitle("Hello world")
+
+            // Read the contents and open its output stream for writing, then
+            // write a short message.
+            Contents originalContents = contentsResult.getContents();
+            OutputStream os = originalContents.getOutputStream();
+            try {
+                os.write("Hello world!\n".getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            // Create the metadata for the new file including title and MIME
+            // type.
+            MetadataChangeSet originalMetadata = new MetadataChangeSet.Builder()
+                    .setTitle("AsyncTaskFile.txt")
                     .setMimeType("text/plain").build();
+
+            // Create the file in the root folder, again calling await() to
+            // block until the request finishes.
             DriveFolder rootFolder = Drive.DriveApi.getRootFolder(getGoogleApiClient());
             DriveFileResult fileResult = rootFolder.createFile(
-                    getGoogleApiClient(), changeSet, contentsResult.getContents()).await();
+                    getGoogleApiClient(), originalMetadata, originalContents).await();
             if (!fileResult.getStatus().isSuccess()) {
+                // We failed, stop the task and return.
                 return null;
             }
-            return fileResult.getDriveFile();
+
+            // Finally, fetch the metadata for the newly created file, again
+            // calling await to block until the request finishes.
+            MetadataResult metadataResult = fileResult.getDriveFile()
+                    .getMetadata(getGoogleApiClient())
+                    .await();
+            if (!metadataResult.getStatus().isSuccess()) {
+                // We failed, stop the task and return.
+                return null;
+            }
+            // We succeeded, return the newly created metadata.
+            return metadataResult.getMetadata();
         }
 
         @Override
-        protected void onPostExecute(DriveFile result) {
+        protected void onPostExecute(Metadata result) {
             super.onPostExecute(result);
             if (result == null) {
-                showMessage("Error while creating the file");
+                // The creation failed somehow, so show a message.
+                showMessage("Error while creating the file.");
                 return;
             }
+            // The creation succeeded, show a message.
             showMessage("File created: " + result.getDriveId());
         }
     }
