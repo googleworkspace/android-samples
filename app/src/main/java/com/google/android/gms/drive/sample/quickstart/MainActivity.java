bda30cc1b6ccf44b3fecaac365f4cfe43167c45e
Arco Xu <arcoxu@google.com>
2017-08-01 15:08:44 -0500

    Convert the app to use connetionless client
    Change-Id: I3d8e25ac31ae0b29f0c61185691c658f52b1cd2a

--- app/src/main/java/com/google/android/gms/drive/sample/quickstart/MainActivity.java
+++ app/src/main/java/com/google/android/gms/drive/sample/quickstart/MainActivity.java
@@ -1,22 +1,17 @@
 /**
  * Copyright 2013 Google Inc. All Rights Reserved.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software distributed under the
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
  * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.google.android.gms.drive.sample.quickstart;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -25,169 +20,197 @@ import android.content.IntentSender.SendIntentException;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.drive.CreateFileActivityOptions;
 import com.google.android.gms.drive.Drive;
-import com.google.android.gms.drive.DriveApi.DriveContentsResult;
+import com.google.android.gms.drive.DriveClient;
+import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveResourceClient;
 import com.google.android.gms.drive.MetadataChangeSet;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
- * Android Drive Quickstart activity. This activity takes a photo and saves it
- * in Google Drive. The user is prompted with a pre-made dialog which allows
- * them to choose the file location.
+ * Android Drive Quickstart activity. This activity takes a photo and saves it in Google Drive. The
+ * user is prompted with a pre-made dialog which allows them to choose the file location.
  */
-public class MainActivity extends Activity implements ConnectionCallbacks,
-        OnConnectionFailedListener {
+public class MainActivity extends Activity {
 
-    private static final String TAG = "drive-quickstart";
-    private static final int REQUEST_CODE_CAPTURE_IMAGE = 1;
-    private static final int REQUEST_CODE_CREATOR = 2;
-    private static final int REQUEST_CODE_RESOLUTION = 3;
+  private static final String TAG = "drive-quickstart";
+  private static final int REQUEST_CODE_SIGN_IN = 0;
+  private static final int REQUEST_CODE_CAPTURE_IMAGE = 1;
+  private static final int REQUEST_CODE_CREATOR = 2;
 
-    private GoogleApiClient mGoogleApiClient;
-    private Bitmap mBitmapToSave;
+  private GoogleSignInClient mGoogleSignInClient;
+  private DriveClient mDriveClient;
+  private DriveResourceClient mDriveResourceClient;
+  private Bitmap mBitmapToSave;
 
-    /**
-     * Create a new file and save it to Drive.
-     */
-    private void saveFileToDrive() {
-        // Start by creating a new contents, and setting a callback.
-        Log.i(TAG, "Creating new contents.");
-        final Bitmap image = mBitmapToSave;
-        Drive.DriveApi.newDriveContents(mGoogleApiClient)
-                .setResultCallback(new ResultCallback<DriveContentsResult>() {
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    signIn();
+  }
 
-            @Override
-            public void onResult(DriveContentsResult result) {
-                // If the operation was not successful, we cannot do anything
-                // and must
-                // fail.
-                if (!result.getStatus().isSuccess()) {
-                    Log.i(TAG, "Failed to create new contents.");
-                    return;
-                }
-                // Otherwise, we can write our data to the new contents.
-                Log.i(TAG, "New contents created.");
-                // Get an output stream for the contents.
-                OutputStream outputStream = result.getDriveContents().getOutputStream();
-                // Write the bitmap data from it.
-                ByteArrayOutputStream bitmapStream = new ByteArrayOutputStream();
-                image.compress(Bitmap.CompressFormat.PNG, 100, bitmapStream);
-                try {
-                    outputStream.write(bitmapStream.toByteArray());
-                } catch (IOException e1) {
-                    Log.i(TAG, "Unable to write file contents.");
-                }
-                // Create the initial metadata - MIME type and title.
-                // Note that the user will be able to change the title later.
-                MetadataChangeSet metadataChangeSet = new MetadataChangeSet.Builder()
-                        .setMimeType("image/jpeg").setTitle("Android Photo.png").build();
-                // Create an intent for the file chooser, and start it.
-                IntentSender intentSender = Drive.DriveApi
-                        .newCreateFileActivityBuilder()
-                        .setInitialMetadata(metadataChangeSet)
-                        .setInitialDriveContents(result.getDriveContents())
-                        .build(mGoogleApiClient);
-                try {
-                    startIntentSenderForResult(
-                            intentSender, REQUEST_CODE_CREATOR, null, 0, 0, 0);
-                } catch (SendIntentException e) {
-                    Log.i(TAG, "Failed to launch file chooser.");
-                }
-            }
-        });
+  /** Start sign in activity. */
+  private void signIn() {
+    Log.i(TAG, "Start sign in");
+    mGoogleSignInClient = buildGoogleSignInClient();
+    startActivityForResult(mGoogleSignInClient.getSignInIntent(), REQUEST_CODE_SIGN_IN);
+  }
+
+  /** Build a Google SignIn client. */
+  private GoogleSignInClient buildGoogleSignInClient() {
+    GoogleSignInOptions signInOptions =
+        new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestScopes(Drive.SCOPE_FILE)
+            .build();
+    return GoogleSignIn.getClient(this, signInOptions);
+  }
+
+  /** Create a new file and save it to Drive. */
+  private void saveFileToDrive() {
+    // Start by creating a new contents, and setting a callback.
+    Log.i(TAG, "Creating new contents.");
+    final Bitmap image = mBitmapToSave;
+
+    mDriveResourceClient
+        .createContents()
+        .continueWithTask(
+            new Continuation<DriveContents, Task<IntentSender>>() {
+              @Override
+              public Task<IntentSender> then(@NonNull Task<DriveContents> task) throws Exception {
+                return createFileIntentSender(task.getResult(), image);
+              }
+            })
+        .addOnFailureListener(
+            new OnFailureListener() {
+              @Override
+              public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Failed to create new contents.", e);
+              }
+            });
+  }
+
+  /**
+   * Creates an {@link IntentSender} to start a dialog activity with configured {@link
+   * CreateFileActivityOptions} for user to create a new photo in Drive.
+   */
+  private Task<Void> createFileIntentSender(DriveContents driveContents, Bitmap image) {
+    Log.i(TAG, "New contents created.");
+    // Get an output stream for the contents.
+    OutputStream outputStream = driveContents.getOutputStream();
+    // Write the bitmap data from it.
+    ByteArrayOutputStream bitmapStream = new ByteArrayOutputStream();
+    image.compress(Bitmap.CompressFormat.PNG, 100, bitmapStream);
+    try {
+      outputStream.write(bitmapStream.toByteArray());
+    } catch (IOException e) {
+      Log.w(TAG, "Unable to write file contents.", e);
     }
 
-    @Override
-    protected void onResume() {
-    	super.onResume();
-    	if (mGoogleApiClient == null) {
-    		// Create the API client and bind it to an instance variable.
-            // We use this instance as the callback for connection and connection
-            // failures.
-            // Since no account name is passed, the user is prompted to choose.
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addApi(Drive.API)
-                    .addScope(Drive.SCOPE_FILE)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .build();
-    	}
-        // Connect the client. Once connected, the camera is launched.
-        mGoogleApiClient.connect();
-    }
+    // Create the initial metadata - MIME type and title.
+    // Note that the user will be able to change the title later.
+    MetadataChangeSet metadataChangeSet =
+        new MetadataChangeSet.Builder()
+            .setMimeType("image/jpeg")
+            .setTitle("Android Photo.png")
+            .build();
+    // Set up options to configure and display the create file activity.
+    CreateFileActivityOptions createFileActivityOptions =
+        new CreateFileActivityOptions.Builder()
+            .setInitialMetadata(metadataChangeSet)
+            .setInitialDriveContents(driveContents)
+            .build();
 
-    @Override
-    protected void onPause() {
-        if (mGoogleApiClient != null) {
-            mGoogleApiClient.disconnect();
+    return mDriveClient
+        .newCreateFileActivityIntentSender(createFileActivityOptions)
+        .continueWith(
+            new Continuation<IntentSender, Void>() {
+              @Override
+              public Void then(@NonNull Task<IntentSender> task) throws Exception {
+                startIntentSenderForResult(task.getResult(), REQUEST_CODE_CREATOR, null, 0, 0, 0);
+                return null;
+              }
+            });
+  }
+
+  @Override
+  protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    switch (requestCode) {
+      case REQUEST_CODE_SIGN_IN:
+        Log.i(TAG, "Sign in request code");
+        // Called after user is signed in.
+        if (resultCode == RESULT_OK) {
+          Log.i(TAG, "Signed in successfully.");
+          // Execute task to get sign in account.
+          Task<GoogleSignInAccount> task =
+              mGoogleSignInClient.getGoogleSignInAccountFromIntent(data);
+          updateViewWithGoogleSignInAccountTask(task);
         }
-        super.onPause();
-    }
-
-    @Override
-    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-        switch (requestCode) {
-            case REQUEST_CODE_CAPTURE_IMAGE:
-                // Called after a photo has been taken.
-                if (resultCode == Activity.RESULT_OK) {
-                    // Store the image data as a bitmap for writing later.
-                    mBitmapToSave = (Bitmap) data.getExtras().get("data");
-                }
-                break;
-            case REQUEST_CODE_CREATOR:
-                // Called after a file is saved to Drive.
-                if (resultCode == RESULT_OK) {
-                    Log.i(TAG, "Image successfully saved.");
-                    mBitmapToSave = null;
-                    // Just start the camera again for another photo.
-                    startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE),
-                            REQUEST_CODE_CAPTURE_IMAGE);
-                }
-                break;
+        break;
+      case REQUEST_CODE_CAPTURE_IMAGE:
+        Log.i(TAG, "capture image request code");
+        // Called after a photo has been taken.
+        if (resultCode == Activity.RESULT_OK) {
+          Log.i(TAG, "Image captured successfully.");
+          // Store the image data as a bitmap for writing later.
+          mBitmapToSave = (Bitmap) data.getExtras().get("data");
+          saveFileToDrive();
         }
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        // Called whenever the API client fails to connect.
-        Log.i(TAG, "GoogleApiClient connection failed: " + result.toString());
-        if (!result.hasResolution()) {
-            // show the localized error dialog.
-            GoogleApiAvailability.getInstance().getErrorDialog(this, result.getErrorCode(), 0).show();
-            return;
+        break;
+      case REQUEST_CODE_CREATOR:
+        Log.i(TAG, "creator request code");
+        // Called after a file is saved to Drive.
+        if (resultCode == RESULT_OK) {
+          Log.i(TAG, "Image successfully saved.");
+          mBitmapToSave = null;
+          // Just start the camera again for another photo.
+          startActivityForResult(
+              new Intent(MediaStore.ACTION_IMAGE_CAPTURE), REQUEST_CODE_CAPTURE_IMAGE);
         }
-        // The failure has a resolution. Resolve it.
-        // Called typically when the app is not yet authorized, and an
-        // authorization
-        // dialog is displayed to the user.
-        try {
-            result.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
-        } catch (SendIntentException e) {
-            Log.e(TAG, "Exception while starting resolution activity", e);
-        }
+        break;
     }
+  }
 
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        Log.i(TAG, "API client connected.");
-        if (mBitmapToSave == null) {
-        	// This activity has no UI of its own. Just start the camera.
-            startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE),
-                    REQUEST_CODE_CAPTURE_IMAGE);
-            return;
-        }
-        saveFileToDrive();
-    }
-
-    @Override
-    public void onConnectionSuspended(int cause) {
-        Log.i(TAG, "GoogleApiClient connection suspended");
-    }
+  /**
+   * Update view with Google SignIn account task. If task was successful, build the drive client and
+   * drive resource client, then start the camera. Otherwise print out the exception stacktrace.
+   */
+  private void updateViewWithGoogleSignInAccountTask(Task<GoogleSignInAccount> task) {
+    Log.i(TAG, "Update view with sign in account task");
+    task.addOnSuccessListener(
+            new OnSuccessListener<GoogleSignInAccount>() {
+              @Override
+              public void onSuccess(GoogleSignInAccount googleSignInAccount) {
+                Log.i(TAG, "Sign in success");
+                // Build a drive client.
+                mDriveClient = Drive.getDriveClient(getApplicationContext(), googleSignInAccount);
+                // Build a drive resource client.
+                mDriveResourceClient =
+                    Drive.getDriveResourceClient(getApplicationContext(), googleSignInAccount);
+                // Start camera.
+                startActivityForResult(
+                    new Intent(MediaStore.ACTION_IMAGE_CAPTURE), REQUEST_CODE_CAPTURE_IMAGE);
+              }
+            })
+        .addOnFailureListener(
+            new OnFailureListener() {
+              @Override
+              public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Sign in failed", e);
+              }
+            });
+  }
 }
